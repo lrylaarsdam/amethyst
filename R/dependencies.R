0862750f190e351a6f3309df3fc65fb8b5392107
Stephen Coleman <stcolema@tcd.ie>
2024-03-01 13:35:20 -0800

    Comment out everything - installation should not automatically do these things as it seems to cause issues within R

--- R/dependencies.R
+++ R/dependencies.R
@@ -26,7 +26,7 @@
 # library("purrr")
 # library("dplyr")
 # library(doParallel)
-
-pal <- c("#004A4A", "#F05252", "#419e9e", "#fcba2b", "#bd083e", "#FB9A99", "#75C8D2",  "#FF8B73", "#B2DF8A", "#1F78B4", "#E31A1C",  "#aae3e3",  "#FFA976")
-options(scipen = 999)
-options(future.globals.maxSize = (20000*1024^2)) # 10 GB
+#
+# pal <- c("#004A4A", "#F05252", "#419e9e", "#fcba2b", "#bd083e", "#FB9A99", "#75C8D2",  "#FF8B73", "#B2DF8A", "#1F78B4", "#E31A1C",  "#aae3e3",  "#FFA976")
+# options(scipen = 999)
+# options(future.globals.maxSize = (20000*1024^2)) # 10 GB
