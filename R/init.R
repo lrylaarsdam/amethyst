f95c6c699928ecfff90824b913d53e4e8275677f
lrylaarsdam <136643355+lrylaarsdam@users.noreply.github.com>
2025-10-23 17:07:43 -0400

    Fix attach message

--- R/init.R
+++ R/init.R
@@ -9,5 +9,5 @@
   options(future.globals.maxSize = (4000*1024^2)) # 4 GB
   cat("Welcome to Amethyst! If you use Amethyst in your work, please cite our associated manuscript:
       Rylaarsdam, L.E. et al. Single-cell DNA methylation analysis tool Amethyst resolves distinct non-CG methylation patterns in human astrocytes and oligodendrocytes.
-      Commun. Biol. 8, 1468 (2025). https://www.nature.com/articles/s42003-025-08859-2")
+      Commun. Biol. 8, 1468 (2025). https://www.nature.com/articles/s42003-025-08859-2 \n")
 }
